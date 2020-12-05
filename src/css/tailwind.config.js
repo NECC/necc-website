5c7550dfe9d47cb8f2e3a5079cff134ff3e3d481
David Machado <codeanywhere.acc.inf@gmail.com>
2020-12-05 14:11:04 +0000

    Add icons Separated js and html minnified css and js

---
+++ src/css/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: [],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {
+      backgroundColor: ['active']
+    },
+  },
+  plugins: [],
+}
