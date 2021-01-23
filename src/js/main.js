60b34c6b18483721fb5616dff3bc5ce09165c059
David Machado <codeanywhere.acc.inf @gmail.com>
2021-01-23 18:14:31 +0000

    Correçao da sala
    remove scrollspy

--- src/js/main.js
+++ src/js/main.js
@@ -9,37 +9,11 @@ function ready() {
         document.querySelector('html').classList.add('dark');
         
         let elem = document.getElementById('darkmode');
-        console.log(elem);
         elem.firstElementChild.classList.add("icon-sunny-outline");
         elem.firstElementChild.classList.remove("icon-moon-outline");
     } else {
         document.querySelector('html').classList.remove('dark')
     }
-
-    let mainNavLinks = document.querySelectorAll("nav div div a");
-
-    window.addEventListener("scroll", event => {
-        let fromTop = window.scrollY;
-
-        mainNavLinks.forEach(link => {
-            let section = document.querySelector(link.hash);
-
-            if (
-                section.offsetTop <= fromTop &&
-                section.offsetTop + section.offsetHeight > fromTop
-            ) {
-                link.classList.add("bg-gray-50");
-                link.classList.add("dark:bg-gray-800");
-                link.classList.add("text-blue-500");
-                link.classList.add("dark:text-blue-400");
-            } else {
-                link.classList.remove("bg-gray-50");
-                link.classList.remove("dark:bg-gray-800");
-                link.classList.remove("text-blue-500");
-                link.classList.remove("dark:text-blue-400");
-            }
-        });
-    });
 }
 
 function navbarToggle() {
