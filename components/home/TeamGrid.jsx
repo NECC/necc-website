837fec85371f8cf59cdfe81ac3fc22c3d69d6b18
tuai <a102938@alunos.uminho.pt>
2023-04-12 11:50:57 +0100

    URL parameters fix

--- components/home/TeamGrid.jsx
+++ components/home/TeamGrid.jsx
@@ -1,10 +1,11 @@
 import Image from 'next/image'
-import {members} from '../../data/members.json'
+import members from '../../data/members.json'
 
 function MobileCard({ path, name, role }) {
+    const decodedPath = decodeURIComponent(path);
     return (
         <figure className="bg-white dark:bg-gray-600 shadow-lg rounded-xl md:rounded-md p-0 overflow-hidden flex flex-row justify-center items-center m-2">
-            <Image src={path} alt="" width={100} height={100} />
+            <Image src={decodedPath} alt="" width={100} height={100} />
             <figcaption className="p-4">
                 <h3 className="font-semibold text-gray-700 dark:text-gray-200 mb-1 w-36">{ name }</h3>
                 <span className="rounded-full bg-blue-50 dark:bg-blue-200 text-sm py-1 px-2 font-semibold text-blue-700 dark:text-blue-900">{ role }</span>
@@ -14,10 +15,11 @@ function MobileCard({ path, name, role }) {
 }
 
 function MemberCard({ path, name, role }) {
+    const decodedPath = decodeURIComponent(path);
     return (
         <div className="bg-white dark:bg-gray-600 shadow-lg rounded-md p-5 flex flex-col justify-evenly items-center m-2">
             <div className="rounded-full border-2 border-gray-200 w-24 h-24">
-                <Image className="rounded-full" src={path} alt="" width={100} height={100} />
+                <Image className="rounded-full" src={decodedPath} alt="" width={100} height={100} />
             </div>
             <h3 className="font-semibold text-gray-700 dark:text-gray-200">{ name }</h3>
             <span className="rounded-full bg-blue-50 dark:bg-blue-200 text-sm py-1 px-2 font-semibold text-blue-700 dark:text-blue-900">{ role }</span>
@@ -26,28 +28,26 @@ function MemberCard({ path, name, role }) {
 }
 
 export default function TeamGrid() {
-    const caminho = image.queryStringParameters.caminho || image;
-
     return (
         <>
             <div className="flex flex-row flex-wrap justify-evenly md:hidden">
                 { 
                     members.map((member) => {
-                            const {caminho, name, role} = member
-                            return(
-                                <MobileCard path={caminho} name={name} role={role} />
-                            );
+                        const {image, name, role} = member;
+                        return (
+                            <MobileCard path={image} name={name} role={role} />
+                        );
                     })
                 }
             </div>
             <div className="hidden md:grid">
                 <div className="grid md:grid-cols-4 lg:grid-cols-5 md:grid-rows-2 lg:grid-rows-1">
                     {   members.map((member) => {
-                            const {caminho, name, role} = member
-                            return(
-                                <MemberCard path={caminho} name={name} role={role} />
-                            );
-                        })
+                        const {image, name, role} = member;
+                        return (
+                            <MemberCard path={image} name={name} role={role} />
+                        );
+                    })
                     }        
                 </div>
             </div>
